b45cf53f82e8a80ec17485b048c1b4535f155985
hudaifa123 <hodaeyfha@gmail.com>
2023-06-09 07:52:04 +0300

    id url

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from products.views import list_view, detail_view, detail_slug_view
 app_name = 'products'
 urlpatterns = [
     path('list/', list_view, name='product_list'),
-    path('detail/<int:object_id>/', detail_view, name='product_detail'),
+    path('details/<int:object_id>/', detail_view, name='product_detail'),
     path('detail/<str:slug>/', detail_slug_view, name='product_detail_slug'),
 ]
